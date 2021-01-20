76dda3f82f98492b678f10156495c0bb8af75c58
snghnishant <blackmalware@protonmail.com>
2021-01-20 22:36:47 +0530

    Added debouncing to autocomplete

--- autocomplete.js
+++ autocomplete.js
@@ -1,41 +1,51 @@
 let sugApi = "https://api.datamuse.com/";
 let sugEndpoint = "sug?s=";
 const suglimit = "&max=10";
-const sugQuery = document.getElementById('query');
-const suggestions = document.getElementById('suggestions');
+const sugQuery = document.getElementById("query");
+const suggestions = document.getElementById("suggestions");
+
+// Debouncing variable
+let timeoutFunction;
 
 let mySug = (search) => {
-    return search.split(' ').join('+');
-}
+	return search.split(" ").join("+");
+};
 
-const displaySug = (sugRes) =>{
-    if(sugRes.length){
-        const sugHtml = sugRes.map(
-            sugReults =>`<p id="suggestion-result" onclick="replaceText(event)">
+const displaySug = (sugRes) => {
+	if (sugRes.length) {
+		const sugHtml = sugRes
+			.map(
+				(
+					sugReults
+				) => `<p id="suggestion-result" onclick="replaceText(event)">
             ${sugReults.word}</p>`
-        ).join('');
-    suggestions.innerHTML = sugHtml;
-    }
-    if(sugQuery.value.length === 0){
-        suggestions.innerHTML = '';
-    }
-}
+			)
+			.join("");
+		suggestions.innerHTML = sugHtml;
+	}
+	if (sugQuery.value.length === 0) {
+		suggestions.innerHTML = "";
+	}
+};
 
-const querySug = async () => {
-    if(sugQuery.value.length){
-        let url = sugApi+sugEndpoint+mySug(sugQuery.value)+suglimit;
-        const sugResponse = await fetch(url);
-        const sugjsonResponse = await sugResponse.json();
-        displaySug(sugjsonResponse);
-        console.log(sugjsonResponse);
-    }
-}
+const querySug = () => {
+	if (timeoutFunction) {
+		clearTimeout(timeoutFunction);
+	}
+	timeoutFunction = setTimeout(async () => {
+		if (sugQuery.value.length) {
+			let url = sugApi + sugEndpoint + mySug(sugQuery.value) + suglimit;
+			const sugResponse = await fetch(url);
+			const sugjsonResponse = await sugResponse.json();
+			displaySug(sugjsonResponse);
+			console.log(sugjsonResponse);
+		}
+	}, 500);
+};
 
+sugQuery.addEventListener("input", querySug);
 
-sugQuery.addEventListener('input', querySug);
-
-
-function replaceText(e){
-    sugQuery.value = e.target.innerText;
-    suggestions.innerHTML = '';
+function replaceText(e) {
+	sugQuery.value = e.target.innerText;
+	suggestions.innerHTML = "";
 }
